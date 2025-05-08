ab9d68a4e085f73a44106111347556f36851defd
md-siam-islam <mdsiamislam663@gmail.com>
2025-05-08 17:31:56 +0600

    All Problem solve Done

---
+++ src/index.ts
@@ -0,0 +1,178 @@
+{
+    // -------------------> problem 1 Start ----------------------->
+
+    function formatString(input: string, toUpper?: boolean): string {
+        if (toUpper !== false) {
+            return input.toLocaleUpperCase();
+        } else {
+            return input.toLocaleLowerCase();
+        }
+    }
+
+    formatString("siam")
+
+     // -------------------> problem 1 End ----------------------->
+
+
+      // -------------------> problem 2 Start ----------------------->
+
+
+      function filterByRating(items: { title: string; rating: number }[]):{ title: string; rating: number }[]{
+
+        return items.filter((item) => item.rating >= 4)
+        
+      }
+      const books = [
+        { title: "Book A", rating: 4.5 },
+        { title: "Book B", rating: 3.2 },
+        { title: "Book C", rating: 5.0 }
+      ];
+
+      filterByRating(books)
+      
+      // -------------------> problem 2 end----------------------->
+
+
+      // -------------------> problem 3 Start ----------------------->
+
+      function concatenateArrays<T>(...arrays: T[][]): T[] {
+        return arrays.reduce((frist , secend) => frist.concat(secend) , [])
+      }
+      
+       // -------------------> problem 3 end ----------------------->
+
+
+        // -------------------> problem 4 Start ----------------------->
+
+        class Vehicle {
+           private make : string
+           private year : number
+
+            constructor ( make : string , year: number){
+                this.make = make
+                this.year = year
+            }
+
+           public getInfo () : string {
+                return `Make: ${this.make}, Year: ${this.year}`;
+            }
+        }
+
+        class Car extends Vehicle {
+            private model : string 
+
+            constructor(make : string , year : number , model : string){
+                super(make , year)
+                this.model = model
+                }
+
+                public getModel(): string {
+                    return `Model: ${this.model}`;
+                
+            }
+        }
+
+        const myCar = new Car("Toyota", 2020, "Corolla");
+        myCar.getInfo()
+        myCar.getModel()
+
+        // -------------------> problem 4 end ----------------------->
+
+        // -------------------> problem 5 start----------------------->
+
+        function processValue(value: string | number): number {
+            if(typeof value === "string"){
+                return value.length
+            }
+            else {
+            return value*2
+            }
+        }
+
+        processValue("hello"); 
+        processValue(10);
+
+        // -------------------> problem 5 end----------------------->
+
+
+
+        // -------------------> problem 6 start----------------------->
+
+
+        interface Product {
+            name: string;
+            price: number;
+          }
+          
+          function getMostExpensiveProduct(products: Product[]): Product | null {
+            if (products.length === 0) {
+              return null;
+            }
+          
+            let maxProduct = products[0];
+          
+            for (let i = 1; i < products.length; i++) {
+              if (products[i].price > maxProduct.price) {
+                maxProduct = products[i];
+              }
+            }
+          
+            return maxProduct;
+          }
+
+          const products = [
+            { name: "Pen", price: 10 },
+            { name: "Notebook", price: 25 },
+            { name: "Bag", price: 50 }
+          ];
+          
+          getMostExpensiveProduct(products);  
+
+      // -------------------> problem 6 end-----------------------> 
+
+      // -------------------> problem 7 start----------------------->
+      
+      enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+      }
+      
+      function getDayType(day: Day): string {
+        if (day === Day.Saturday || day === Day.Sunday) {
+          return "Weekend";
+        } else {
+          return "Weekday";
+        }
+      }
+
+      getDayType(Day.Monday); 
+
+      getDayType(Day.Sunday); 
+
+     // -------------------> problem 7 end----------------------->
+
+    // -------------------> problem 8 start----------------------->
+
+    async function squareAsync(n: number): Promise<number> {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (n < 0) {
+              reject(new Error("Negative number not allowed"));
+            } else {
+              resolve(n * n);
+            }
+          }, 1000);
+        });
+      }
+
+      squareAsync(4).then(console.log);     
+    squareAsync(-3).catch(console.error); 
+
+// -------------------> problem 8 end----------------------->
+
+}
